50bf636af062f5e4cf27140e379c6374c5d58723
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2024-10-25 12:05:24 +0530

    Create sumofzero.c

---
+++ sumofzero.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+int n;
+printf("enter n");
+scanf("%d",&n);
+    if(n>0)
+    {
+    printf("positive");
+}
+else if(n==0)
+{
+printf("zero");
+}
+        else
+        {
+        printf("negitive");
+    }
+}
