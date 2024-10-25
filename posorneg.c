e0490be811f964590312ca9f2b56a7de26f568c9
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2024-10-25 12:01:47 +0530

    Create posorneg.c

---
+++ posorneg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+int n;
+printf("enter n");
+scanf("%d",&n);
+    if(n>0)
+    {
+    printf("positive");
+}
+        else
+        {
+        printf("negitive");
+    }
+}
