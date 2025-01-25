754221a2aa48900bd976131a22bd9382fd583393
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-01-25 10:39:37 +0530

    Create myexp4.c

---
+++ myexp4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int x=10,c;
+    c=x;
+    printf("%d\n",c);
+    c+=x;
+    printf("%d\n",c);
+    c-=x;
+    printf("%d\n",c);
+    c*=x;
+    printf("%d\n",c);
+    c/=x;
+    printf("%d\n",c);
+    c%=x;
+}
