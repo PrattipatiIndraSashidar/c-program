fc31f4ad70ecc89eccb2c3d3c10a8f339df2216c
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-01-25 10:55:33 +0530

    Create myexp5.c

---
+++ myexp5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+   int x,y;
+   printf("enter amount of ravi and raju:");
+   scanf("%d\n",&x,&y);
+   if(x>y)
+   {
+       printf("%d ravi as more money",x);
+   }
+   else{
+       printf("%d raju as more money",y);
+   }
+   }
