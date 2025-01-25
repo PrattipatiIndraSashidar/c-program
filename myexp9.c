17e7f814684054f5ece9fc697b34dfc47a04605c
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-01-25 11:34:45 +0530

    Create myexp9.c

---
+++ myexp9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,num;
+    printf("enter any number:");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
