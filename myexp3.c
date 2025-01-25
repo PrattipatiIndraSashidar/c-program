a654e849d5723d8df69658487a573651d9d59352
PrattipatiIndraSashidar <indra.prathipati05@gmail.com>
2025-01-25 10:33:20 +0530

    Create myexp3.c

---
+++ myexp3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a=20,b=30;
+    printf("%d\n",a+b);
+     printf("%d\n",a-b);
+      printf("%d\n",a*b);
+       printf("%d\n",a/b);
+        printf("%d\n",a%b);
+}
